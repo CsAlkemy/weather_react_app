2cb49a8439ca0843dbe2f188ff8cbe1aa9603b5e
CsAlkemy <alkemy48@gmail.com>
2021-02-07 00:07:19 +0600

    final

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import SearchLoc from "./components/SearchLoc";
 import { API_KEY, URL } from "./apis/config";
 import WeatherCard from "./components/WeatherCard";
 import WeatherDetails from "./components/WeatherDetails";
+import Loader from "../src/image/partly-cloudy-day-rain.svg";
+import ReactIC from '../src/image/react.png';
+import TailwindIC from '../src/image/tailwindcss.svg';
+import WeatherIc from '../src/image/weather.svg';
+import Axioss from '../src/image/axios.svg'
 
 function App() {
-  const { data, error,  setUrl } = useFetch();
+  const { data, error, isLoading, setUrl } = useFetch();
 
   const getContent = () => {
     if (error)
@@ -15,8 +20,18 @@ function App() {
           className="bg-pink-200 border-l-4 border- rounded  border-red-600 mt-10 pt-4 pb-4 "
           role="alert"
         >
-          <h1 className='ml-5 pl-4'>Error when featching: {error}</h1>
-          <p className='ml-5 pl-4 text-sm font-thin'>Please re-check your city and Search again.. </p>
+          <h1 className="ml-5 pl-4">Error when featching: {error}</h1>
+          <p className="ml-5 pl-4 text-sm font-thin">
+            Please re-check your city and Search again..{" "}
+          </p>
+        </div>
+      );
+    if (!data && isLoading)
+      return (
+        <div className="flex-1 max-w-5xl p-5 md:p-16 mx-auto">
+          <div>
+            <img className="h-20 mx-auto" src={Loader} alt="loaderr" />
+          </div>
         </div>
       );
     if (!data) return null;
@@ -32,6 +47,13 @@ function App() {
             </div>
           </div>
         </div>
+        <div className="flex max-w-lg p-5 md:p-16 mx-auto">
+          <img  className='h-7 mx-auto ' src={ReactIC} alt="a"/>
+          <img  className='h-7  mx-auto' src={TailwindIC} alt="a"/>
+          <img  className='h-7 mx-auto' src={WeatherIc} alt="a"/>
+          <img  className='h-7  mx-auto' src={Axioss} alt="a"/>
+        </div>
+        
       </>
     );
   };
--- src/components/SearchLoc.js
+++ src/components/SearchLoc.js
@@ -10,13 +10,12 @@ const SearchLoc = ({ onSearch }) => {
   //   setQuery("");
   // };
 
-
   const [city, setCity] = useState("");
   console.log(city);
   return (
     <div className="flex w-11/12 md:w-1/3 mx-auto">
       <input
-        className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10"
+        className="focus:border-light-blue-600 focus:ring-4 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-3 pl-10"
         type="text"
         onChange={(e) => setCity(e.target.value)}
         value={city}
@@ -26,14 +25,15 @@ const SearchLoc = ({ onSearch }) => {
           }
         }}
         aria-label="Filter projects"
-        placeholder="Search Location"
+        placeholder="Search Weather by City"
         required
       />
       <button
-        className="bg-purple-600 pr-2 pl-2 text-base text-white rounded-lg ml-2"
+        className="bg-purple-600 pr-4 pl-4 text-base text-white rounded-lg ml-2"
         onClick={() => onSearch(city)}
       >
         Check
+        <span className="animate-ping absolute inline-flex h-4 w-4 rounded-full bg-gray-100 opacity-75"></span>
       </button>
     </div>
   );
